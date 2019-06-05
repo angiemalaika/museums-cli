086b6d3977e8ad90537404bb992b5d8038b9e44b
Angela Mumba <malaika.mumba@gmail.com>
2019-06-05 21:33:30 +0000

    Attraction list Done!

--- lib/museums/cli.rb
+++ lib/museums/cli.rb
@@ -4,23 +4,25 @@ require 'nokogiri'
 require "museums/cli/version"
 require 'pry'
 
-def run
-  puts "Welcome to the Smithsonian"
-  puts "Here is a menu of our Musuems, Galleries and Zoo"
-  puts "---------------------------------------------------"
+class CLI
+  def run
+    attractions = []
+    
+    puts "Welcome to the Smithsonian"
+    puts "Here is a menu of our Musuems, Galleries and Zoo"
+    puts "---------------------------------------------------"
+    
+    html = open("https://www.si.edu/museums")
+    doc = Nokogiri::HTML(html)
+    
+    doc.css(".title.h3").each do |attraction|
+    name = attraction.text.strip
+    
+    attractions << name 
+      end 
+    attractions.each.with_index(1) do |name,i|
+      puts "#{i}. #{name}"
   
-  html = open("https://www.si.edu/museums")
-  doc = Nokogiri::HTML(html)
-  
-  doc.css(".title.h3").each do |attraction|
-  title = attraction.text.strip
-  binding.pry 
-  end 
+      end 
+  end
 end
-
-# module Museums
-#   module Cli
-#     class Error < StandardError; end
-#     # Your code goes here...
-#   end
-# end
