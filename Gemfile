0edd359d91cb4e9b9de731eb96cb59b40f2d6e66
Angela Mumba <malaika.mumba@gmail.com>
2019-06-05 20:49:20 +0000

    first

--- Gemfile
+++ Gemfile
@@ -1,4 +1,8 @@
 source "https://rubygems.org"
+gem 'pry'
+gem 'nokogiri'
+
+
 
 # Specify your gem's dependencies in museums-cli.gemspec
 gemspec
--- lib/museums/cli.rb
+++ lib/museums/cli.rb
@@ -1,8 +1,18 @@
 require "museums/cli/version"
 
-module Museums
-  module Cli
-    class Error < StandardError; end
-    # Your code goes here...
-  end
+require "museums/cli/version"
+
+def run
+  puts "Welcome to the Smithsonian"
+  puts "Here is a menu of our Musuems, Galleries and Zoo"
+  puts "---------------------------------------------------"
+  
+  
 end
+
+# module Museums
+#   module Cli
+#     class Error < StandardError; end
+#     # Your code goes here...
+#   end
+# end
