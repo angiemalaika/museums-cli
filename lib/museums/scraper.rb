76181083f1dbe2ee770709dd16b741fcaf859378
Angela Mumba <malaika.mumba@gmail.com>
2019-06-06 22:06:44 +0000

    re-writting second task

--- lib/museums/scraper.rb
+++ lib/museums/scraper.rb
@@ -12,10 +12,28 @@ class Scraper
     
     doc.css(".title.h3").each do |attraction|
     name = attraction.text.strip
+    doc.css(".location").each do|x|
+    x.text.delete("\n").strip
+    
      Attraction.new(name)
-    # binding.pry
+   # doc.css(".location").text.delete("\n").strip
+   
+     binding.pry
    end 
  end
 end 
+# doc.css(".location").each do |loc|
+#     m_location = loc.text.delete("\n").strip
+#       m_location
+#       delete("\n").strip
+#     [22] pry(Scraper)> doc.css('.location').map(&:text).first.strip
+# => "15th St. and Constitution Ave., NW Washington, DC                 \n                          See on Map"
 
-    
+# god.delete("\n").strip.first
+
+
+# doc.css(".location")[1].text.delete("\n")
+# => "                    950 Independence Avenue, SW Washington, DC                                                        See on Map
+
+pry(Scraper)> doc.css(".location")[1].text.delete("\n").strip
+=> "950 Independence Avenue, SW Washington, DC                                    See on Map"
