16a3c644b4c16632d40c7e4569aabea3ff63bc0d
Angela Mumba <malaika.mumba@gmail.com>
2019-06-06 16:09:16 +0000

    diplay with OOO

--- lib/museums/cli.rb
+++ lib/museums/cli.rb
@@ -1,32 +1,48 @@
 require "museums/cli/version"
 require 'pry'
-require 'open-uri'
-require 'nokogiri'
+# require 'open-uri'
+# require 'nokogiri'
 
 class CLI
+  
   def run
-     attractions = []
     
     puts "Welcome to the Smithsonian Museums, Galleries and Zoo."
     puts "Here is a menu of our exhibits and attractions:"
     puts "---------------------------------------------------"
     
-    html = open ("https://www.si.edu/museums")
-    doc = Nokogiri::HTML(html)
-    
-    doc.css(".title.h3").each do |attraction|
-    name = attraction.text.strip
-     #Attraction.new(name)
-    # binding.pry
-    attractions << name 
+      Scraper.scrape_attractions
+      self.display_attractions 
+      puts "Please type a number to view the museums location"
+      puts "To quit, type 'exit'."
+      input = ""
+      input = gets.strip
   end 
-  #   Scraper.scrape_attractions
-  #   display_attractions
-  # end 
-    
-  # def display_attractions
-      attractions.each.with_index(1) do |name,i|
-      puts "#{i}. #{name}"
+  
+  def display_attractions 
+      Attraction.all.each.with_index(1) do |exhibit,i|
+      puts "#{i}. #{exhibit.name}"
       end 
-   end 
-end
+  end 
+end 
+  
+   
+   
+    #attractions = []
+    #   html = open ("https://www.si.edu/museums")
+  #   doc = Nokogiri::HTML(html)
+    
+  #   doc.css(".title.h3").each do |attraction|
+  #   name = attraction.text.strip
+  #   Attraction.new(name)
+     
+  #   # binding.pry
+  # end 
+
+   
+  # puts "Please type a number to view the museums location"
+  # puts "To quit, type 'exit'."
+      # input = ""
+    # input = gets.strip
+   # if  input != "exit"
+
